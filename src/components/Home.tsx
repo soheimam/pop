69eb13891c74012037967160ad1906ec14dbe63d
Soheima Maradiaga <soheimam@gmail.com>
2023-10-06 22:02:15 +0200

    added homepage component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,12 @@ import { Button } from "@/components/ui/button";
 
 export default function Home({ title = "" }) {
   return (
-    <main>
-      <h1> WOOOOOOOOOO POP</h1>
-      <Button>Click me</Button>
+    <main className="mx-10  lg:mx-24">
+      <div className="grid col-span-6 bg-blue-500 py-12 px-8 lg:col-span-12">
+        <h2 className="pb-2 text-3xl font-semibold   text-white text-wrap col-span-4">
+          You snap it! You sell it
+        </h2>
+      </div>
     </main>
   );
 }
