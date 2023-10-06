a4ee7465d2fa24eb2b8e2fe37f1f69b63d59edca
Soheima Maradiaga <soheimam@gmail.com>
2023-10-06 20:44:31 +0200

    added shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
   themeColor: "#ffffff",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home({ title = "" }) {
   return (
     <main>
       <h1> WOOOOOOOOOO POP</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
