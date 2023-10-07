3e830be93e46db285b4842c3751d6a85df56f74c
Soheima Maradiaga <soheimam@gmail.com>
2023-10-07 16:12:38 +0200

    added manifest

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  publicRuntimeConfig: {
+    manifestPath: "/manifest.json",
+  },
 };
 
 const withPWA = require("next-pwa")({
   dest: "public",
+  publicExcludes: ["**/manifest.json"],
 });
 
 module.exports = withPWA(nextConfig);
