ac76409d75b40c5ad439938efc29839a63db9016
Daniel Huffer <danielhuffer@live.com>
2023-10-10 03:01:59 +1000

    forge

---
+++ chain/scripts/ComputeRegistryAddress.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// import "forge-contracts/contracts/Script.sol";
+// import "@openzeppelin/contracts/utils/Create2.sol";
+import "../contracts/ERC6551RegistryImpl.sol"
+
+contract ComputeRegistryAddress {
+    function run() external view {
+        address registry = Create2.computeAddress(
+            0x6551655165516551655165516551655165516551655165516551655165516551,
+            keccak256(type(ERC6551RegistryImpl).creationCode),
+            0x4e59b44847b379578588920cA78FbF26c0B4956C
+        );
+
+        console.log(registry);
+ 
+    }
+}
