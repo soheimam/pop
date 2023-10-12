641dc20654a473310213a49d78b6c90ced704b61
Soheima Maradiaga <soheimam@gmail.com>
2023-10-12 11:59:14 +0200

    return data from car api in the ui

--- frontend/src/app/notifications/[id]/page.tsx
+++ frontend/src/app/notifications/[id]/page.tsx
@@ -1,10 +1,50 @@
+import { NotificationItem, chainNameType } from "@pushprotocol/uiweb";
+import * as PushAPI from "@pushprotocol/restapi";
+import { useWeb3React } from "@web3-react/core";
+
+const { account, library, chainId } = useWeb3React();
+const _signer = library.getSigner(account);
+
+const notifications = await PushAPI.user.getFeeds({
+  user: "eip155:5:0xD8634C39BBFd4033c0d3289C4515275102423681", // user address in CAIP
+  env: "staging",
+});
+
 function Page({ params }: { params: { id: string } }) {
   // If the profile is not found, display a message
   console.log(params, "notifications");
 
   return (
     <main>
-      <h1> Notis</h1>
+      {notifications.map((oneNotification, i) => {
+        const {
+          cta,
+          title,
+          message,
+          app,
+          icon,
+          image,
+          url,
+          blockchain,
+          notification,
+        } = oneNotification;
+
+        return (
+          <NotificationItem
+            key={id} // any unique id
+            notificationTitle={title}
+            notificationBody={message}
+            cta={cta}
+            app={app}
+            icon={icon}
+            image={image}
+            url={url}
+            theme={theme}
+            chainName={blockchain}
+            // chainName={blockchain as chainNameType} // if using Typescript
+          />
+        );
+      })}
     </main>
   );
 }
